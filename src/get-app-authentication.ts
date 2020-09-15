b2a256a49c40860e81d56f7483713c5a27cb5d3b
Chris Opperwall <copperwall@gmail.com>
2020-09-15 13:56:21 -0700

    fix(app authentication): Handle time difference between system and GitHub API time in JWT claims

--- src/get-app-authentication.ts
+++ src/get-app-authentication.ts
@@ -1,12 +1,17 @@
 import { githubAppJwt } from "universal-github-app-jwt";
 
-import { AppAuthentication } from "./types";
+import { AppAuthentication, State } from "./types";
 
-export async function getAppAuthentication(
-  id: number,
-  privateKey: string
-): Promise<AppAuthentication> {
-  const appAuthentication = await githubAppJwt({ id, privateKey });
+export async function getAppAuthentication({
+  id,
+  privateKey,
+  timeDifference,
+}: State): Promise<AppAuthentication> {
+  const appAuthentication = await githubAppJwt({
+    id,
+    privateKey,
+    now: timeDifference && Math.floor(Date.now() / 1000) + timeDifference,
+  });
 
   return {
     type: "app",
