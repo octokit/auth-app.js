414137da55d05af0835faf99438f52f2f24047ac
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-31 12:57:33 -0700

    test: reproduction for #268

--- test/typescript-validate.ts
+++ test/typescript-validate.ts
@@ -6,16 +6,24 @@ import { createAppAuth } from "../src";
 
 function expectType<T>(what: T) {}
 
-export async function readmeExample() {
-  const auth = createAppAuth({
-    appId: 1,
-    privateKey: "-----BEGIN PRIVATE KEY-----\n...",
-    clientId: "lv1.1234567890abcdef",
-    clientSecret: "1234567890abcdef12341234567890abcdef1234",
-  });
+const auth = createAppAuth({
+  appId: 1,
+  privateKey: "-----BEGIN PRIVATE KEY-----\n...",
+  clientId: "lv1.1234567890abcdef",
+  clientSecret: "1234567890abcdef12341234567890abcdef1234",
+});
 
+export async function readmeExample() {
   // Retrieve an oauth-access token
   const userAuthentication = await auth({ type: "oauth-user", code: "123456" });
 
   expectType<"token">(userAuthentication.type);
+}
+
+export async function issue268() {
+  // Retrieve an oauth-access token
+  const userAuthentication = await auth({ type: "installation" });
+
+  expectType<"token">(userAuthentication.type);
+  expectType<"tokenType">(userAuthentication.tokenType);
 }
