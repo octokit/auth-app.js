93bb5761923572defebfcbbf580e8e58a4ba2732
wolfy1339 <webmaster@wolfy1339.com>
2024-03-05 13:15:47 -0500

    test: add type for `clock` variable

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import fetchMock, { type MockMatcherFunction } from "fetch-mock";
 
 import { request } from "@octokit/request";
-import { install } from "@sinonjs/fake-timers";
+import { install, type InstalledClock } from "@sinonjs/fake-timers";
 import { jest } from "@jest/globals";
 
 import { createAppAuth, createOAuthUserAuth } from "../src/index.ts";
@@ -39,7 +39,7 @@ x//0u+zd/R/QRUzLOw4N72/Hu+UG6MNt5iDZFCtapRaKt6OvSBwy8w==
 const BEARER =
   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOi0zMCwiZXhwIjo1NzAsImlzcyI6MX0.q3foRa78U3WegM5PrWLEh5N0bH1SD62OqW66ZYzArp95JBNiCbo8KAlGtiRENCIfBZT9ibDUWy82cI4g3F09mdTq3bD1xLavIfmTksIQCz5EymTWR5v6gL14LSmQdWY9lSqkgUG0XCFljWUglEP39H4yeHbFgdjvAYg3ifDS12z9oQz2ACdSpvxPiTuCC804HkPVw8Qoy0OSXvCkFU70l7VXCVUxnuhHnk8-oCGcKUspmeP6UdDnXk-Aus-eGwDfJbU2WritxxaXw6B4a3flTPojkYLSkPBr6Pi0H2-mBsW_Nvs0aLPVLKobQd4gqTkosX3967DoAG8luUMhrnxe8Q";
 
-let clock: any;
+let clock: InstalledClock;
 beforeEach(() => {
   clock = install({ now: 0, toFake: ["Date", "setTimeout"] });
 });
