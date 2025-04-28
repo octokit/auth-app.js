07cc972584222f149ee451516727bb26a7bd71a2
dudu.cpp <eduardo@emaclab.com>
2025-04-28 08:33:14 -0300

    feat: handling exp is too far in the future

--- src/hook.ts
+++ src/hook.ts
@@ -16,7 +16,9 @@ import type {
 const FIVE_SECONDS_IN_MS = 5 * 1000;
 
 function isNotTimeSkewError(error: RequestError) {
-  return !(
+  return !(error.message.match(
+      /'Expiration time' claim \('exp'\) is too far in the future/,
+    ) ||
     error.message.match(
       /'Expiration time' claim \('exp'\) must be a numeric value representing the future time at which the assertion expires/,
     ) ||
